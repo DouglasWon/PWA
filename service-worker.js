0dd0a5ae553a8659081e7ddb540a1dd8a7f0c1df
DouglasWon <deokyoun.won@gmail.com>
2019-10-10 00:19:18 +0900

    del

--- service-worker.js
+++ service-worker.js
@@ -23,18 +23,18 @@ self.addEventListener('fetch', function(event) {
    );
 });
 
-self.addEventListener('activate', function(event) {
-   event.waitUntil(
-     caches.keys().then(function(cacheNames) {
-       return Promise.all(
-         cacheNames.map(function(cacheName) {
-             if (cacheWhitelist.indexOf(cacheName) === -1 ) {
-	            return caches.delete(cacheName);
-             }         
-         })
-       );
-     })
-   );
-}); 
+// self.addEventListener('activate', function(event) {
+//    event.waitUntil(
+//      caches.keys().then(function(cacheNames) {
+//        return Promise.all(
+//          cacheNames.map(function(cacheName) {
+//              if (cacheWhitelist.indexOf(cacheName) === -1 ) {
+// 	            return caches.delete(cacheName);
+//              }         
+//          })
+//        );
+//      })
+//    );
+// }); 
 
 
